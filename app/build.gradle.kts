18a226d5859e0ac1bbbc99d1ce489434b7f71d53
itskartik51 <kartikey.toda@gmail.com>
2026-09-05 09:18:28 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,5 +75,4 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended:1.7.0")
 
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("io.coil-kt:coil-compose:2.4.0")
 }
