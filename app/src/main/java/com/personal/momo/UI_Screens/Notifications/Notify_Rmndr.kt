496ad0d69217805950f7b50cd5b689a76ce15753
itskartik51 <kartikey.toda@gmail.com>
2026-09-19 09:18:46 +0530

    Create Notify_Rmndr.kt

---
+++ app/src/main/java/com/personal/momo/UI_Screens/Notifications/Notify_Rmndr.kt
@@ -0,0 +1,8 @@
+package com.personal.momo.UI_Screens.Notifications
+
+/**
+ * Placeholder for future notification reminders functionality.
+ */
+object NotifyReminderPlaceholder {
+    // Future reminder definitions and handlers will be implemented here
+}
