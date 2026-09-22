a308456a78e210249218d5a99b7b53cc1323f43c
itskartik51 <kartikey.toda@gmail.com>
2026-09-22 13:24:00 +0530

    Create CallSounds.kt

---
+++ app/src/main/java/com/personal/momo/UI_Screens/Call/CallSounds.kt
@@ -0,0 +1,53 @@
+package com.personal.momo.UI_Screens.Call
+
+import android.content.Context
+import android.media.AudioManager
+import android.media.Ringtone
+import android.media.RingtoneManager
+import android.media.ToneGenerator
+
+object CallSounds {
+    private var toneGenerator: ToneGenerator? = null
+    private var incomingRingtone: Ringtone? = null
+
+    fun startDialTone() {
+        try {
+            stopDialTone()
+            toneGenerator = ToneGenerator(AudioManager.STREAM_VOICE_CALL, 80)
+            toneGenerator?.startTone(ToneGenerator.TONE_SUP_RINGTONE)
+        } catch (_: Exception) {
+        }
+    }
+
+    fun stopDialTone() {
+        try {
+            toneGenerator?.stopTone()
+            toneGenerator?.release()
+        } catch (_: Exception) {
+        }
+        toneGenerator = null
+    }
+
+    fun startIncomingRingtone(context: Context) {
+        try {
+            stopIncomingRingtone()
+            val uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
+            incomingRingtone = RingtoneManager.getRingtone(context.applicationContext, uri)
+            incomingRingtone?.play()
+        } catch (_: Exception) {
+        }
+    }
+
+    fun stopIncomingRingtone() {
+        try {
+            incomingRingtone?.stop()
+        } catch (_: Exception) {
+        }
+        incomingRingtone = null
+    }
+
+    fun releaseAll() {
+        stopDialTone()
+        stopIncomingRingtone()
+    }
+}
