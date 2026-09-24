e23a45582fc0c37a1b76dd5a35a9de1219f7ceca
itskartik51 <kartikey.toda@gmail.com>
2026-09-24 11:24:51 +0530

    Update build.gradle.kts [RUN]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.personal.momo"
         minSdk = 24
         targetSdk = 34
-        versionCode = 20
-        versionName = "1.2405.2"
+        versionCode = 21
+        versionName = "1.2405.21"
 
         resourceConfigurations += listOf("en", "hi")
 
